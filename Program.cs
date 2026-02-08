bf1dcbf84711b44e8d259c79531d6b87efebbe74
awsedrggft <danilsemagaev8@gmail.com>
2026-02-08 13:27:13 +1000

    Force public database URL

--- Program.cs
+++ Program.cs
@@ -12,32 +12,17 @@ var builder = WebApplication.CreateBuilder(args);
 var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
 string connectionString;
 
-if (!string.IsNullOrEmpty(databaseUrl))
-{
-    Console.WriteLine(" DATABASE_URL found, converting...");
-    
-    var uri = new Uri(databaseUrl);
-    var userInfo = uri.UserInfo.Split(':');
-    var host = uri.Host;
-    var port = uri.Port;
-    var database = uri.AbsolutePath.TrimStart('/');
-    var username = userInfo[0];
-    var password = userInfo.Length > 1 ? userInfo[1] : "";
-    
-    connectionString = $"Host={host};Port={port};Database={database};Username={username};Password={password};SSL Mode=Require;Trust Server Certificate=true";
-    
-    Console.WriteLine($" Database: {database} at {host}:{port}");
-}
-else
-{
-    Console.WriteLine(" DATABASE_URL not found, using appsettings.json");
-    connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-}
+// FORCE PUBLIC URL (temporary fix)
+Console.WriteLine(" Using HARDCODED public database URL");
+connectionString = "Host=shuttle.proxy.rlwy.net;Port=59015;Database=railway;Username=postgres;Password=SDcVqugRuVEDlJUtzsMPpHgXnlaUBYn;SSL Mode=Require;Trust Server Certificate=true";
+
+Console.WriteLine($" Connection: shuttle.proxy.rlwy.net:59015");
 
 if (string.IsNullOrEmpty(connectionString))
 {
     throw new InvalidOperationException(" No database connection found!");
 }
+
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(connectionString));
